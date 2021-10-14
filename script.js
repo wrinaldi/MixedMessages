1abc9d97e976d07c394ae1635cac64ff815ac62d
wrinaldi <willrinaldi1@gmail.com>
2021-10-13 22:32:02 -0400

    added functionality cads q esc p

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-console.log('its working');
-console.log('on the next branch');
+//console.log('its working');
+//console.log('on the next branch');
+
+const randMessage = {
+    greeting: ['hi', 'hello'],
+    intro: [' buddy', ' pal', ' guy'],
+    type: [' i need a new mtb', ' ya boy is looking for a cruiser', ' need something for hh'],
+    pay: [' willing to pay $5', ' willing to ball out']
+
+};
+function printMsg(){
+    let str = '';
+    for (let key in randMessage){
+        str = str + randMessage[key][Math.floor(Math.random()*randMessage[key].length)];
+        console.log(key);
+    };
+    console.log(str);
+};
+
+printMsg();
